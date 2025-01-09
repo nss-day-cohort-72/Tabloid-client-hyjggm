47bd1292880ac3b7ee8418b9e623aaf4d32f1087
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2025-01-09 11:08:25 -0600

    Completed deactivating user

--- src/managers/userProfileManager.js
+++ src/managers/userProfileManager.js
@@ -1,9 +1,18 @@
-const _apiUrl = "/api/userprofile";
+const _apiUrl = '/api/userprofile';
 
 export const getProfiles = () => {
-  return fetch(_apiUrl + "/withroles").then((res) => res.json());
+  return fetch(_apiUrl + '/withroles').then((res) => res.json());
 };
 
 export const getProfile = (id) => {
   return fetch(_apiUrl + `/${id}`).then((res) => res.json());
+};
+
+export const deactivateUserProfile = (id) => {
+  return fetch(`${_apiUrl}/deactivate/${id}`, {
+    method: 'PUT', // Specify the HTTP method
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 };
